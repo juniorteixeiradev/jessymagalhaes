fdc7df5a9bf20b2d1471968a7571786ef1174037
teixeirajrwd <teixeirajrwd@gmail.com>
2023-12-28 00:00:18 -0300

    Navbar animation add

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,7 +1,6 @@
 import styles from "./Header.module.css";
 import handleScroll from "./functions/fixedNavbar";
-import useScrollReveal from "../../core/useScrollReveal";
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import { Link } from "react-scroll";
 import { animated, useSpring } from "@react-spring/web";
 import { MdCancelPresentation } from "react-icons/md";
@@ -11,15 +10,28 @@ export default function Nav(){
     const [mobile, setMobile] = useState();
     const [display, setDisplay] = useState();
     
+    const springs = useSpring({
+        from:{
+            height:0
+        }, 
+        to:{
+            height:210
+        },
+        config:{
+            duration:100
+        },
+        reset: true
+    })
     function handleClick (sinal){
         console.log("clicou no hamburguer");
     
         if (sinal === "open") {
             setDisplay("flex")
         } else if (sinal === "close") { 
-            setDisplay("none") }
+            setDisplay("none") 
+        }
     }
-    function verificaRes (){
+    const verificaRes = useCallback(() =>{
         const res = window.innerWidth;
 
         if(res <= 1000){
@@ -30,7 +42,7 @@ export default function Nav(){
             setMobile(false)
         }
         return res;
-    }
+    },[]);
 
     
     useEffect(() => {
@@ -52,14 +64,15 @@ export default function Nav(){
         {mobile === true && 
         <>
             <div id="hamburguer" className={styles.navbartrigger} onClick={() => {handleClick("open")}}>☰</div>
+            
             <ul style={{display: display}}>
-  
+                <animated.div style={springs}>
                 <li><Link to="topper" spy={true} smooth={true} href="#" onClick={()=>{handleClick("close")}}>Home</Link></li>
                 <li><Link to="sobre" spy={true} smooth={true} href="#sobre" onClick={()=>{handleClick("close")}}>Sobre</Link></li>
                 <li><Link to="contato" spy={true} smooth={true} href="#contato" onClick={()=>{handleClick("close")}}>Contato</Link></li>
                 <li><Link to="locais" spy={true} smooth={true} href="#locais" onClick={()=>{handleClick("close")}}>Locais de atendimento</Link></li>
-                <div style={{display: "flex" }} className={styles.navbartrigger} onClick={()=>{handleClick("close")}}><MdCancelPresentation/></div>
-
+                <div style={{display: "flex", justifyContent:"center", cursor:"pointer" }} onClick={()=>{handleClick("close")}}><MdCancelPresentation/></div>
+                </animated.div>
             </ul>
         </>
         }
